1cc36c4f1adba77f32bf6fee50c3ca8060b3a4be
Prasanna <pvenkat4ever@gmail.com>
2021-05-23 01:16:18 +0530

    styled landing page for the mac

--- src/components/landing/ContentArea.tsx
+++ src/components/landing/ContentArea.tsx
@@ -1,11 +1,38 @@
 const AboutMe = () => {
-    return <div> About me </div>
-}
+    return <div className="rounded shadow-xl h-64 p-4 w-full">
+        Some text about me...
+        Some text about me...
+        Some text about me...
+        Some text about me...
+        Some text about me...
+        Some text about me...
+        Some text about me...
+        Some text about me...
 
-const ContentArea = () => {
-    return <div>
-        <AboutMe/>
     </div>
 }
 
-export default ContentArea;
+const TalksAndBlogs = () => {
+    return <div className="w-1/2 rounded shadow-xl p-4 h-32 border-4 mt-4">
+        Talks & Blogs
+    </div>
+}
+
+const Social = () => {
+    return <div className="w-full rounded shadow-xl p-4 h-32 border-4 mt-4">
+        Social
+    </div>
+}
+
+const Container = () => {
+    return <div className="p-6 -mt-20 mr-20 ml-20 bg-white shadow-xl rounded-xl flex flex-col items-center space-x-4">
+        <AboutMe/>
+        <div className="flex flex-row justify-between w-full">
+            <TalksAndBlogs/>
+            <TalksAndBlogs/>
+        </div>
+        <Social/>
+    </div>
+}
+
+export default Container;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,21 @@ import {getAllPosts} from "../lib/api";
 import Post from "../types/post";
 import Posts from "../components/Posts";
 import Bio from "../components/Bio";
+import ContentArea from "../components/landing/ContentArea";
 
 type Props = {
     allPosts: Post[]
 }
 
 const Home = ({allPosts}: Props) => {
-    return <div>
-        <Bio/>
-        <Posts entries={allPosts}/>
+    return <div className="flex flex-col h-screen">
+        <header className="h-60 bg-black"> Header</header>
+        <div className="flex-1">
+            <ContentArea/>
+        </div>
+        <footer className="py-5 text-center">
+            Hey. I'm a footer.
+        </footer>
     </div>
 }
 
