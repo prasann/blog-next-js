7efd4bd30c27a29a00c931be0552c8228f4815ea
Prasanna <mail.prasanna.v@gmail.com>
2022-02-05 23:57:48 +0530

    added alt for profile log

--- src/components/posts/FooterCard.tsx
+++ src/components/posts/FooterCard.tsx
@@ -3,7 +3,7 @@ import profileImage from './../../../public/assets/profile.jpg';
 
 const FooterCard = () => {
     return <footer className="items-center flex flex-col md:flex-row justify-center justify-items-center my-4 display-block">
-        <Image className="rounded-full" src={profileImage} width="64" height="64"/>
+        <Image alt="profile-image" className="rounded-full" src={profileImage} width="64" height="64"/>
         <div className="mt-4 md:mt-0 md:w-6/12 text-center"> Prasanna is a full stack web developer, with exposure to various programming languages.
             Uses mostly Java, Javascript these days and got ~13 years of architecting and coding enterprise software solutions. </div>
     </footer>
