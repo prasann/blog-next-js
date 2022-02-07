d8ad71d227ff4759f9f6ef2a6416e291f628641f
Prasanna <mail.prasanna.v@gmail.com>
2022-02-07 17:59:50 +0530

    added menu bar to the top

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -10,7 +10,7 @@ const PostListItem = ({title, description, slug, date, minutesToRead}: Post) => 
 
     return <div key={slug}
                 onClick={() => navigateTo(slug)} className="cursor-pointer m-1 p-2 border-b-2 hover:bg-gray-100">
-        <div className="text-center md:text-left my-2 text-sky-400 text-2xl hover:text-red-500">{title}</div>
+        <div className="text-center md:text-left my-2 text-sky-400 text-2xl hover:text-green-500">{title}</div>
         <div className="flex flex-row justify-between">
             <div className="text-gray-500 italic text-sm">{date}</div>
             <div className="text-gray-500 italic text-sm">{minutesToRead}</div>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,17 @@ import {useRouter} from "next/router";
 import Image from 'next/image';
 import logoImage from './../../../public/assets/logo.png';
 
+type MenuLinkProps = {
+    displayName: string,
+    location: string
+}
+
+const MenuLink = ({displayName, location}: MenuLinkProps) => {
+    return <div className="p-2 text-sky-400 hover:text-green-500  hover:cursor-pointer hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white rounded-xl"><a href={location}
+              className="text-xl font-semibold transition duration-300">
+        {displayName}</a></div>
+}
+
 const Header = () => {
     const router = useRouter();
 
@@ -10,9 +21,16 @@ const Header = () => {
     }
 
     return <header className="header dark-background ">
-        <div onClick={navigateToHome} className="cursor-pointer mx-4 my-2">
-            <div className="mt-2 w-6/12 md:w-3/12 xl:w-2/12">
-                <Image src={logoImage} alt="logo" layout="responsive"/>
+        <div className="flex flex-row justify-between">
+            <div onClick={navigateToHome} className="cursor-pointer mx-4 my-2">
+                <div className="mt-2 w-6/12 md:w-4/12 xl:w-3/12">
+                    <Image src={logoImage} alt="logo"/>
+                </div>
+            </div>
+            <div className="hidden md:mr-8 md:flex items-center space-x-2">
+                <MenuLink displayName="About" location="/about"/>
+                <MenuLink displayName="Talks" location="/talks"/>
+                <MenuLink displayName="Blog" location="/blog"/>
             </div>
         </div>
     </header>
