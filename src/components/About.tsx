a95a9c841feec2104ebaf6f56398dd75c7914f86
Prasanna <mail.prasanna.v@gmail.com>
2022-02-09 13:40:49 +0530

    modified the about me page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const About = () => {
@@ -8,12 +9,12 @@ const About = () => {
             I live in Bengaluru, India. Works as a Lead consultant <a href="https://thoughtworks.com:">@thoughtworks</a>
         </div>
         <div className="my-4">
-            I'm a software architect with lots of experience in building high-scale enterprise
+            I'm a software architect with experience in building high-scale enterprise
             applications and retail solutions with agile and extreme programming practices. I have led digital
             transformation and legacy strangulation programs across various industries/sectors.
         </div>
         <div className="my-4">
-            I have done lots of presentation and talks on various tech topics. Please do check out them <a href="/talks"> here </a>
+            I have done few public talks on various tech topics. Please do check out them <Link href={"/talks"}> here </Link>
         </div>
         <div className="my-4">
             Happy to work on exciting things, you can reach me at <span className="font-bold text-twitter-blue"> mail@prasanna.dev</span>
