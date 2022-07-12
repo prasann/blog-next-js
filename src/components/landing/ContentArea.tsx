7329a33cfc265d77ed11b169eb832647988f134a
Prasanna <mail.prasanna.v@gmail.com>
2022-07-12 21:23:00 +0530

    removed few components and added lorem

--- src/components/landing/ContentArea.tsx
+++ src/components/landing/ContentArea.tsx
@@ -5,11 +5,19 @@ import {faTwitter, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icon
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 
 const AboutMe = () => {
-    return <div className="rounded text-xl shadow-xl p-6 text-center">
+    return <div className="rounded text-xl shadow-xl l:m-12 p-6 text-justify leading-loose">
         A computer science engineer, with a master’s degree in software systems.
         A full-stack developer with experience in building and leading development teams that build high-performance enterprise platforms and products.
-        Loves to work in teams and not a 10x programmer <Link href={"/about"}>Know
-        more...</Link>
+        Loves to work in teams and not a 10x programmer <Link href={"/about"}>Know more...</Link>
+        <div>
+        <div></div>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+        
+        The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </div>
+        <div>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+        The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </div>
     </div>
 }
 
@@ -46,18 +54,7 @@ const Social = () => {
 const Container = () => {
     return <div className="content-area bg-white rounded-xl">
         <AboutMe/>
-        <div className="w-full">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-4 mt-4 justify-items-center">
-                <Card title="Talks"
-                      description="My talks"
-                      color="variant1"
-                      path="/talks"/>
-                <Card title="Blog"
-                      description="My blog"
-                      color="variant1"
-                      path="/blog"/>
-            </div>
-        </div>
+        
         {/*<img src="http://ghchart.rshah.org/prasann" alt="prasann's Github chart" />*/}
         <Social/>
     </div>
