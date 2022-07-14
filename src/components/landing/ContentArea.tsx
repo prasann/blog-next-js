c05b85474883c26c14bb6596486b121ba8378db2
Prasanna <mail.prasanna.v@gmail.com>
2022-07-14 11:19:23 +0530

    use local image for profile

--- src/components/landing/ContentArea.tsx
+++ src/components/landing/ContentArea.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link'
 import Card from "./Card";
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import ProfileImage from "./../../images/profile.jpeg";
 
 const AboutMe = () => {
     return <div className="rounded text-xl shadow-xl p-2 leading-relaxed">
         <div className='flex flex-col lg:flex-row mb-4'>
-            <div className='inline lg:w-1/4 lg:mt-12'>
-                <img className="rounded-full" 
-                src="https://avatars.githubusercontent.com/u/380340?v=4" alt="profile pic" />
+            <div className='inline text-center lg:w-1/4 lg:mt-12'>
+                <div className='w-2/3 lg:w-full text-center'>
+                    <Image layout="responsive" placeholder='blur'
+                        sizes='120vw'
+                        className="rounded-full" src={ProfileImage} alt="profile pic" />
+                </div>
             </div>
             <div className='lg:w-3/4 p-4 lg:ml-8 lg:mr-4'>
                 <div className="inline-block animate-wave text-2xl lg:text-6xl origin-[70%_70%]">👋🏽</div>
@@ -37,27 +42,20 @@ const Social = () => {
     return <div className="w-full rounded p-4 mt-4">
         <div className="flex flex-row justify-evenly">
             <a aria-label="github handle prasann"
-                className="text-black highlight-animation text-center" rel="noreferrer" href="https://github.com/prasann" target="_blank">
-                <FontAwesomeIcon aria-hidden={true} icon={faGithub} size="3x" />
+                className="text-black highlight-animation text-center lg:text-2xl" rel="noreferrer" href="https://github.com/prasann" target="_blank">
+                <FontAwesomeIcon aria-hidden={true} icon={faGithub} size="2x" />
             </a>
             <a aria-label="twitter handle pvenk"
-                className="highlight-animation text-center" rel="noreferrer" href="https://twitter.com/pvenk" target="_blank">
-                <FontAwesomeIcon aria-hidden={true} className="text-twitter-blue" icon={faTwitter} size="3x" />
+                className="highlight-animation text-center lg:text-2xl" rel="noreferrer" href="https://twitter.com/pvenk" target="_blank">
+                <FontAwesomeIcon aria-hidden={true} className="text-twitter-blue" icon={faTwitter} size="2x" />
             </a>
             <a aria-label="linkedin handle pvenk"
-                className="highlight-animation text-center" rel="noreferrer" href="https://www.linkedin.com/in/pvenk" target="_blank">
-                <FontAwesomeIcon aria-hidden={true} className="text-linkedin-blue" icon={faLinkedin} size="3x" />
+                className="highlight-animation text-center lg:text-2xl" rel="noreferrer" href="https://www.linkedin.com/in/pvenk" target="_blank">
+                <FontAwesomeIcon aria-hidden={true} className="text-linkedin-blue" icon={faLinkedin} size="2x" />
             </a>
             <a aria-label="email - mail@prasanna.dev"
-                className="text-center hidden md:inline" rel="noreferrer" href="mailto:mail@prasanna.dev" target="_blank">
-                <FontAwesomeIcon aria-hidden={true} className="highlight-animation text-green-600" icon={faEnvelope} size="3x" />
-            </a>
-        </div>
-        <div className="flex-row justify-evenly inline md:hidden mt-4">
-            <a aria-label="email - mail@prasanna.dev"
-                className="text-center" href="mailto:mail@prasanna.dev" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon aria-hidden={true} className="highlight-animation text-green-600" icon={faEnvelope} size="3x" />
-                <div className="mx-1 inline-block">mail@prasanna.dev</div>
+                className="text-center lg:text-2xl" rel="noreferrer" href="mailto:mail@prasanna.dev" target="_blank">
+                <FontAwesomeIcon aria-hidden={true} className="highlight-animation text-green-600" icon={faEnvelope} size="2x" />
             </a>
         </div>
     </div>
