db98906ddeb9c9b48c30e4d6fb07748b7ce9fc2c
Prasanna <mail.prasanna.v@gmail.com>
2023-02-03 18:58:28 +0530

    Upgrading packages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const withPWA = require('next-pwa')
-const prod = process.env.NODE_ENV === 'production'
-
-module.exports = withPWA({
-    images: {
-        domains: ['dev-to-uploads.s3.amazonaws.com'],
-        formats: ['image/webp']
-    },
-    pwa: {
-        disable: !prod,
-        dest: 'public'
-    }
-})
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+  
+  module.exports = withPWA({})
