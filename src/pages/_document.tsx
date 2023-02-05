3530a8ff5b4200123dc3001816e40126411690fc
Prasanna <mail.prasanna.v@gmail.com>
2023-02-05 13:04:18 +0530

    Fix build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
 
                     <link rel='manifest' href='/manifest.json'/>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,600;1,400;1,600&display=swap"
                         rel="stylesheet"/>
