0dbc2f4394a6f42719645487cd00e483e8d21bc5
Prasanna <mail.prasanna.v@gmail.com>
2023-02-11 09:30:55 +0530

    added confetti celebrations

--- src/components/posts/Engage.tsx
+++ src/components/posts/Engage.tsx
@@ -1,39 +1,67 @@
-import {faTwitter} from "@fortawesome/free-brands-svg-icons";
-import {faHeart} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {event} from "../../lib/googleTag";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { event } from "../../lib/googleTag";
+import confetti from "canvas-confetti";
 
 type Props = {
-    description: string,
-}
+  description: string;
+};
 
 const shareOnTwitter = (description: string) => {
-    const url = window.location.href;
-    window.open(`https://twitter.com/intent/tweet?text=${description}&url=${url}`);
-}
+  const url = window.location.href;
+  window.open(
+    `https://twitter.com/intent/tweet?text=${description}&url=${url}`
+  );
+};
 
 const likeThePost = () => {
-    const postName = window.location.href.split("posts/")[1]
-    const gTagEvent = {
-        action: "post_likes",
-        category: "LikeButton",
-        label: postName,
-        value: 1
-    }
-    event(gTagEvent)
-}
+  celebrate();
+  const postName = window.location.href.split("posts/")[1];
+  const gTagEvent = {
+    action: "post_likes",
+    category: "LikeButton",
+    label: postName,
+    value: 1,
+  };
+  event(gTagEvent);
+};
 
-const Engage = ({description}: Props) => {
-    return <div className="bg-white flex flex-row">
-        <div className="mx-2 flex flex-row highlight-animation" onClick={() => shareOnTwitter(description)}>
-            <FontAwesomeIcon className="cursor-pointer text-twitter-blue" icon={faTwitter} size="2x"/>
-            <div className="align-middle mx-2 text-bold">Share</div>
-        </div>
-        <div className="mx-2 flex flex-row highlight-animation" onClick={likeThePost}>
-            <FontAwesomeIcon className="cursor-pointer text-heart-red" icon={faHeart} size="2x"/>
-            <div className="align-middle mx-2 text-bold">Like</div>
-        </div>
+const celebrate = () => {
+    confetti({
+        particleCount: 400,
+        spread: 70,
+        origin: { y: 0.6 }
+      });
+};
+
+const Engage = ({ description }: Props) => {
+  return (
+    <div className="bg-white flex flex-row">
+      <div
+        className="mx-2 flex flex-row highlight-animation"
+        onClick={() => shareOnTwitter(description)}
+      >
+        <FontAwesomeIcon
+          className="cursor-pointer text-twitter-blue"
+          icon={faTwitter}
+          size="2x"
+        />
+        <div className="align-middle mx-2 text-bold">Share</div>
+      </div>
+      <div
+        className="mx-2 flex flex-row highlight-animation"
+        onClick={likeThePost}
+      >
+        <FontAwesomeIcon
+          className="cursor-pointer text-heart-red"
+          icon={faHeart}
+          size="2x"
+        />
+        <div className="align-middle mx-2 text-bold">Like</div>
+      </div>
     </div>
-}
+  );
+};
 
 export default Engage;
