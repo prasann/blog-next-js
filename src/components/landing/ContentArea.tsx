773bb64313f50987a3942b2180b10dc32be5cfcc
Prasanna Nagarajan <mail.prasanna.v@gmail.com>
2025-01-02 10:07:13 +0530

    updating content

--- src/components/landing/ContentArea.tsx
+++ src/components/landing/ContentArea.tsx
@@ -66,13 +66,15 @@ const Writeup = () => {
   return (
     <div className="lg:text-justify">
       <div className="mt-4 lg:mt-8">
-        I'm a full-stack developer hailing from the bustling city of Bengaluru, India. When I'm not busy building and leading development teams to create high-performance enterprise platforms and products, you can find me indulging in my love for coffee and coding (sometimes simultaneously).
+      I'm a full-stack developer from the bustling city of Bengaluru, India—where navigating traffic is almost as challenging as debugging code 🤯 But yes, it's all about finding the right shortcuts, whether on the road or in the code! 
+      
+      I've led large teams to build high-performance enterprise platforms. When I'm not doing that, you'll find me enjoying coffee and coding ☕️
       </div>
       <div className="mt-2">
-        I hold a master's degree in software systems, which is just a fancy way of saying I spent a lot of time in front of a computer screen. Currently, I'm working with the amazing folks at Microsoft, but I used to be a part of the Thoughtworks family. Yes, I’m a certified AWS solution architect, which means I can architect solutions in the cloud while making it rain (metaphorically, of course). Oh, and did I mention I have multiple Azure certifications too? Basically, if it's in the cloud, I've got it covered!
+        I hold a master's degree in software systems, which is just a fancy way of saying I spent a lot of time in front of a computer screen. Currently, I'm working with the amazing folks at Microsoft, but I used to be a part of the Thoughtworks family. I'm a certified AWS solution architect, which means I can architect solutions in the cloud while making it rain (metaphorically, of course). Oh, and did I mention I have multiple Azure certifications too? Basically, if it's in the cloud, I've got it covered!
       </div>
       <div className="mt-2">
-      I have hands-on experience with web technologies, which is a polite way of saying I've broken the internet a few times. Since you're here, why not check out my blogs for some random musings or visit my talks page to hear more about my adventures in tech 🏃‍♂️
+      I have hands-on experience with web technologies, which is a polite way of saying I've broken things on the internet a few times. Since you're here, why not check out my blogs for some random musings or visit my talks page to hear more about my adventures in tech 🏃‍♂️
       </div>
       <div className="mt-8">
         <div className="flex">
